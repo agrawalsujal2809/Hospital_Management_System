a206b23ac09407558cedb8ffdfea7b0a1462af51
Sujal Agrawal <agrawalsujal2809@gmail.com>
2026-06-16 18:53:13 +0530

    Fix patient entity

--- src/main/java/com/hms/hospital_api/entity/Patient.java
+++ src/main/java/com/hms/hospital_api/entity/Patient.java
@@ -1,15 +1,18 @@
 package com.hms.hospital_api.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "PATIENTS")
+@Table(name = "patients")
 public class Patient {
 
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String name;
 
@@ -18,21 +21,17 @@ public class Patient {
     public Patient() {
     }
 
-    public Patient(
-            int id,
-            String name,
-            String disease) {
-
+    public Patient(Integer id, String name, String disease) {
         this.id = id;
         this.name = name;
         this.disease = disease;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -40,9 +39,7 @@ public class Patient {
         return name;
     }
 
-    public void setName(
-            String name) {
-
+    public void setName(String name) {
         this.name = name;
     }
 
@@ -50,9 +47,7 @@ public class Patient {
         return disease;
     }
 
-    public void setDisease(
-            String disease) {
-
+    public void setDisease(String disease) {
         this.disease = disease;
     }
 }
